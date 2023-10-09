145667fb30c8ee3de8ea64f9d85709ec526122cd
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-10-09 18:16:00 +0200

    added-dice-game-code

--- js esercizio-1/app.js
+++ js esercizio-1/app.js
@@ -6,23 +6,64 @@ const mailList = [
     "nonno@gmail.com",
     "nonna@gmail.com"
 ]
-console.log(mailList)
+console.log(mailList);
 
 // - chiedere al utente la sua mail
 const userMail = prompt('Inserisci la tua mail')
-console.log(userMail)
+console.log(userMail);
+
+let access = false;
 
 // - con il sistema for individuare lista dei mail dal array
 for ( let i = 0; i < mailList.length; i++) {
-    console.log(i, mailList[i])
+    console.log(i, mailList[i]);
 
     // - SE la mail è presente nella lista stampare il messaggio positivo
-    if (mailList[i] === userMail) {
-        alert('Benvenuto')
-
-    // ALTRIMENTI negare l'accesso
-    } else {
-        alert('Accesso Negato')
+    
+    const verifyMail = (mailList[i] === userMail);
+    if (verifyMail) {
+        // console.log('benvenuto ' + verifyMail);
+        access = true;
     }
 }
+
+if (access === true) {
+    console.log('accesso consentito');
+
+        // # js-dadi
+    // - generare un numero random da 1/6 per il giocatore
+
+    const userRandom = Math.floor((Math.random() * 6) + 1);
+    console.log(userRandom);
+
+    // - generare un numero random da 1/6 per il computer
+
+    const pcRandom = Math.floor((Math.random() * 6) + 1);
+    console.log(pcRandom);
+
+
+    // - SE il numero del giocatore è più alto allora ha vinto il gicatore
+    let userWin = userRandom > pcRandom;
+    let pcWin = userRandom < pcRandom;
+
+
+    if (userWin) {
+        console.log('userRandom ha vinto');
+
+    // - ALTRIMENTI SE il numero del computer è più altro ha vinto il computer
+
+    } else if (pcWin) {
+        console.log('pcRandom ha vinto');
+
+    // -  ALTRIMENTI il numero è uguale allora è un pareggio
+
+    } else {
+        console.log('è un pareggio')
+    }
+
+} else {
+    alert('accesso negato');
+}
+
+console.log(access);
 
